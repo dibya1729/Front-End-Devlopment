1a723c46f9a99f17abf8a3a416dee5584fb754db
dibya1729 <dibyashasamal6@gmail.com>
2024-08-03 09:55:34 +0530

    My Project

--- Random User Generator/script.js
+++ Random User Generator/script.js
@@ -5,11 +5,11 @@ function fetchRandomUser() {
         .then(response => response.json())
         .then(data => {
             const user = data.results[0];
-            document.getElementById('name').textContent = `${user.name.first} ${user.name.last}`;
+            document.getElementById('name').textContent = `${user.name.first}` `${user,name.last}`;
             document.getElementById('email').textContent = user.email;
             document.getElementById('password').textContent = user.login.password;
             document.getElementById('birthday').textContent = new Date(user.dob.date).toLocaleDateString();
             document.getElementById('phone').textContent = user.phone;
-            document.getElementById('address').textContent = `${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state}, ${user.location.country}`;
+            document.getElementById('address').textContent = `${user.location.street.number}` `${user.location.street.name}`, `${user.location.city}`, `${user.location.state}`, `${user.location.country}`;
         });
 }
